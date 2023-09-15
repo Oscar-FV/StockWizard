96e8cf8416e8541cea6bc16fffd5eea37328c025
Oscar-FV <oscar.flores.vqz@gmail.com>
2023-09-14 21:03:18 -0600

    sidebar structure

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,12 +1,11 @@
 import {
-  Card,
-  Typography,
+  Button,
+  Chip,
+  IconButton,
   List,
   ListItem,
   ListItemPrefix,
   ListItemSuffix,
-  Chip,
-  Collapse,
 } from "@material-tailwind/react";
 import {
   PresentationChartBarIcon,
@@ -20,106 +19,37 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export function Root() {
-  const [open, setOpen] = React.useState(false);
-
-  const toggleOpen = () => setOpen((cur) => !cur);
-
+  //hover:w-[70%] ease-in-out duration-300 hover:col-span-2
   return (
     <div className="grid grid-cols-12">
-      <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-        <List className=" min-w-[40px]">
-          <ListItem>
-            <ListItemPrefix>
-              <PresentationChartBarIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <ShoppingBagIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <InboxIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <UserCircleIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <Cog6ToothIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-          <ListItem>
-            <ListItemPrefix>
-              <PowerIcon className="h-5 w-5" />
-            </ListItemPrefix>
-          </ListItem>
-        </List>
-      </Card>
+      <nav className="border border-blue-600 col-span-1 w-[3.5rem] h-screen grid grid-rows-6 overflow-hidden hover:w-[12rem] ease-in-out duration-300 hover:col-span-2">
+        <div className="row-span-4 mt-6 mx-3 border border-red-400 ">
+        <ul className="list-none space-y-14 font-subtitles">
+          <li className="min-w-max">
+            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
+                <PresentationChartBarIcon className="h-8 w-8" />
+              <span className="-mr-1">Dashboard</span>
+            </a>
+          </li>
+          <li className="min-w-max">
+            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
+                <PresentationChartBarIcon className="h-8 w-8" />
+              <span className="-mr-1">Dashboard</span>
+            </a>
+          </li>
+          <li className="min-w-max">
+            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
+                <PresentationChartBarIcon className="h-8 w-8" />
+              <span className="-mr-1">Dashboard</span>
+            </a>
+          </li>
+        </ul>
+        </div>
+        <div className="row-start-6 row-span-1 border border-red-400"></div>
+      </nav>
       <div className="col-span-11" id="detail">
         <Outlet />
       </div>
     </div>
-
-    //   <Collapse open={open}>
-    //     <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-    //       <div className="mb-2 p-4">
-    //         <Typography variant="h5" color="blue-gray">
-    //           Sidebar
-    //         </Typography>
-    //       </div>
-    //       <List>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <PresentationChartBarIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           Dashboard
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <ShoppingBagIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           E-Commerce
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <InboxIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           Inbox
-    //           <ListItemSuffix>
-    //             <Chip
-    //               value="14"
-    //               size="sm"
-    //               variant="ghost"
-    //               color="blue-gray"
-    //               className="rounded-full"
-    //             />
-    //           </ListItemSuffix>
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <UserCircleIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           Profile
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <Cog6ToothIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           Settings
-    //         </ListItem>
-    //         <ListItem>
-    //           <ListItemPrefix>
-    //             <PowerIcon className="h-5 w-5" />
-    //           </ListItemPrefix>
-    //           Log Out
-    //         </ListItem>
-    //       </List>
-    //     </Card>
-    //   </Collapse>
   );
 }
