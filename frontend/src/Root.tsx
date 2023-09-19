910a45c9fa85f62c280c7caa6c3477ed370e6b96
Oscar-FV <oscar.flores.vqz@gmail.com>
2023-09-19 00:55:07 -0600

    sidebar desing finished

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,53 +1,61 @@
 import {
-  Button,
-  Chip,
-  IconButton,
-  List,
-  ListItem,
-  ListItemPrefix,
-  ListItemSuffix,
-} from "@material-tailwind/react";
-import {
-  PresentationChartBarIcon,
-  ShoppingBagIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
-  InboxIcon,
+  InboxStackIcon,
   PowerIcon,
+  ShoppingBagIcon,
+  UsersIcon
 } from "@heroicons/react/24/solid";
-import React from "react";
 import { Outlet } from "react-router-dom";
 
 export function Root() {
-  //hover:w-[70%] ease-in-out duration-300 hover:col-span-2
   return (
-    <div className="grid grid-cols-12">
-      <nav className="border border-blue-600 col-span-1 w-[3.5rem] h-screen grid grid-rows-6 overflow-hidden hover:w-[12rem] ease-in-out duration-300 hover:col-span-2">
-        <div className="row-span-4 mt-6 mx-3 border border-red-400 ">
-        <ul className="list-none space-y-14 font-subtitles">
-          <li className="min-w-max">
-            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
-                <PresentationChartBarIcon className="h-8 w-8" />
-              <span className="-mr-1">Dashboard</span>
-            </a>
-          </li>
-          <li className="min-w-max">
-            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
-                <PresentationChartBarIcon className="h-8 w-8" />
-              <span className="-mr-1">Dashboard</span>
-            </a>
-          </li>
-          <li className="min-w-max">
-            <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4">
-                <PresentationChartBarIcon className="h-8 w-8" />
-              <span className="-mr-1">Dashboard</span>
-            </a>
-          </li>
-        </ul>
+    <div className="flex">
+      <nav className="w-[3rem] h-screen grid grid-rows-6 overflow-hidden hover:w-[13rem] ease-in-out duration-300 bg-details-light drop-shadow-lg">
+        <div className="flex items-center row-span-4 mt-6">
+          <ul className="list-none space-y-8 font-subtitles min-w-fit w-full text-details">
+            <li className="w-full">
+              <a
+                href="#"
+                aria-label="dashboard"
+                className="py-3 relative flex items-center space-x-4 hover:text-white hover:bg-details hover:w-full"
+              >
+                <InboxStackIcon className="h-6 w-6 ml-[.6rem]" />
+                <span className="-mr-1">Inventario</span>
+              </a>
+            </li>
+            <li className="w-full">
+              <a
+                href="#"
+                aria-label="dashboard"
+                className="py-3 relative flex items-center space-x-4 hover:text-white hover:bg-details hover:w-full"
+              >
+                <UsersIcon className="h-6 w-6 ml-[.6rem]" />
+                <span className="-mr-1">Clientes</span>
+              </a>
+            </li>
+            <li className="w-full">
+              <a
+                href=""
+                aria-label="dashboard"
+                className="py-3 relative flex items-center space-x-4 hover:text-white hover:bg-details hover:w-full"
+              >
+                <ShoppingBagIcon className="h-6 w-6 ml-[.6rem]" />
+                <span className="-mr-1">Venta</span>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="row-start-6 row-span-1 border border-red-400"></div>
+        <div className="flex items-end min-w-max row-start-6 mt-6 font-subtitles  text-details">
+          <a
+            href=""
+            aria-label="dashboard"
+            className="py-3 relative flex items-center space-x-4 hover:text-white hover:bg-details hover:w-full"
+          >
+            <PowerIcon className="h-5 w-5 ml-3" />
+            <span>Cerrar Sesión</span>
+          </a>
+        </div>
       </nav>
-      <div className="col-span-11" id="detail">
+      <div className="w-full" id="detail">
         <Outlet />
       </div>
     </div>
